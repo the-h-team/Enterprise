8429383273e96065b5cade148df9b35a68a8b5f9
Matt <58913953+ms5984@users.noreply.github.com>
2021-11-09 08:15:45 -0500

    project: meta, api changes
    * License headers in API module (Apache 2.0)
    * Author tags
    * Re-scope AbstractPlayerEntity (make public; actually flesh out javadoc); Update EnterpriseEntity accordingly

--- Enterprise-API/src/main/java/com/github/sanctum/economy/construct/assets/DecimalAsset.java
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/construct/assets/DecimalAsset.java
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.sanctum.economy.construct.assets;
 
 import com.github.sanctum.economy.construct.Amount;
@@ -9,6 +24,7 @@ import java.math.BigDecimal;
  * Marks an asset that can be divided by BigDecimal.
  *
  * @since 2.0.0
+ * @author ms5984
  */
 public interface DecimalAsset {
     /**
--- Enterprise-API/src/main/java/com/github/sanctum/economy/construct/system/Queryable.java
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/construct/system/Queryable.java
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.sanctum.economy.construct.system;
 
 import com.github.sanctum.economy.construct.Amount;
@@ -7,6 +22,7 @@ import org.jetbrains.annotations.NotNull;
  * A point that can be tested for the holding of assets.
  *
  * @since 2.0.0
+ * @author ms5984
  */
 public interface Queryable {
     /**
