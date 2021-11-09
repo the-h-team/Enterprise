9e3863b1d00a737941379d7b223825ac7da2e772
Matt <58913953+ms5984@users.noreply.github.com>
2021-11-09 08:15:45 -0500

    project: meta, api changes
    * License headers in API module (Apache 2.0)
    * Author tags
    * Re-scope AbstractPlayerEntity (make public; actually flesh out javadoc); Update EnterpriseEntity accordingly

--- Enterprise-API/src/main/java/com/github/sanctum/economy/construct/assets/Asset.java
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/construct/assets/Asset.java
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.sanctum.economy.construct.assets;
 
 import org.bukkit.Material;
@@ -17,6 +32,7 @@ import java.util.regex.Pattern;
  * Identifies a specific asset that may be holdable and/or tradable.
  *
  * @since 2.0.0
+ * @author ms5984
  */
 public class Asset {
     /**
@@ -110,6 +126,7 @@ public class Asset {
      * Native implementation of item-to-asset mappings.
      *
      * @since 2.0.0
+     * @author ms5984
      */
     public static class Items {
         static final Items INSTANCE = new Items();
--- Enterprise-API/src/main/java/com/github/sanctum/economy/construct/entity/EnterpriseEntity.java
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/construct/entity/EnterpriseEntity.java
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.sanctum.economy.construct.entity;
 
 import org.bukkit.OfflinePlayer;
@@ -11,6 +26,7 @@ import java.util.regex.Pattern;
  * Identifies an economically-involved entity.
  *
  * @since 2.0.0
+ * @author ms5984
  */
 public class EnterpriseEntity {
     /**
@@ -118,8 +134,9 @@ public class EnterpriseEntity {
      * Native implementation for players by UniqueId.
      *
      * @since 2.0.0
+     * @author ms5984
      */
-    public static class PlayerByUUID extends AbstractPlayerEntity implements PlayerEntity {
+    public static class PlayerByUUID extends AbstractPlayerEntity {
 
         PlayerByUUID(OfflinePlayer player) {
             super("p_uid", player.getUniqueId().toString(), player);
@@ -150,8 +167,9 @@ public class EnterpriseEntity {
      * Native implementation for players by username.
      *
      * @since 2.0.0
+     * @author ms5984
      */
-    public static class PlayerByUsername extends AbstractPlayerEntity implements PlayerEntity {
+    public static class PlayerByUsername extends AbstractPlayerEntity {
 
         PlayerByUsername(OfflinePlayer player) {
             super("p_username", player.getName(), player);
