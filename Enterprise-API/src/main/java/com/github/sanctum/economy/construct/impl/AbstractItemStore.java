349f0b5a57621a088a4480121dfe82bda872a315
Matt <58913953+ms5984@users.noreply.github.com>
2021-11-09 21:04:46 -0500

    feat: provide new general impls
    * AnyCurrencyWallet: Store any AbstractCurrency-based asset
    * AbstractItemStore: Store MaterialAssets and MetaItemAssets
    * Add necessary returns to avoid falling through to the throws (SingleCurrencyWallet#give,#set,#take)
    * Fix SingleCurrencyWallet#take (copied give + forgot to change); Initialize SingleCurrencyWallet#amount (cannot be null!)
    * Merge PlayerEntity and AbstractPlayerEntity: remove PlayerEntity + copy method javadocs to AbstractPlayerEntity methods

---
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/construct/impl/AbstractItemStore.java
@@ -0,0 +1,91 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.github.sanctum.economy.construct.impl;
+
+import com.github.sanctum.economy.construct.Amount;
+import com.github.sanctum.economy.construct.assets.*;
+import com.github.sanctum.economy.construct.system.Balance;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+/**
+ * An object suitable for managing amounts of item assets.
+ *
+ * @since 2.0.0
+ * @author ms5984
+ */
+public class AbstractItemStore implements Balance {
+    final HashMap<String, Integer> counts = new HashMap<>();
+
+    @Override
+    public boolean has(@NotNull Amount amount) {
+        if (amount instanceof ItemAmount) {
+            return counts.getOrDefault(amount.getAsset().getFQN(), 0) >= ((ItemAmount) amount).getIntegralAmount();
+        }
+        return false;
+    }
+
+    @Override
+    public void give(@NotNull Amount amount) throws AcceptError {
+        if (amount instanceof ItemAmount) {
+            synchronized (counts) {
+                counts.merge(amount.getAsset().getFQN(), ((ItemAmount) amount).getIntegralAmount(), Integer::sum);
+            }
+            return;
+        }
+        throw new AcceptError(amount, "This asset is not supported.");
+    }
+
+    @Override
+    public void set(@NotNull Amount amount) throws SetError {
+        if (amount instanceof ItemAmount) {
+            synchronized (counts) {
+                counts.put(amount.getAsset().getFQN(), ((ItemAmount) amount).getIntegralAmount());
+            }
+            return;
+        }
+        throw new SetError(amount, "This asset is not supported.");
+    }
+
+    @Override
+    public void take(@NotNull Amount amount) throws SupplyError {
+        if (amount instanceof ItemAmount) {
+            final int newAmount = ((ItemAmount) amount).getIntegralAmount();
+            final String fqn = amount.getAsset().getFQN();
+            synchronized (counts) {
+                if (counts.getOrDefault(fqn, 0) < newAmount) {
+                    throw new SupplyError(amount, "Insufficient amount!");
+                }
+                counts.put(fqn, newAmount);
+            }
+            return;
+        }
+        throw new SupplyError(amount, "This asset is not supported.");
+    }
+
+    @Override
+    public @NotNull Optional<Amount> total(@NotNull Asset asset) {
+        if (asset instanceof MaterialAsset || asset instanceof MetaItemAsset) {
+            final Integer integer = counts.get(asset.getFQN());
+            if (integer != null) {
+                return Optional.of(((IntegralAsset) asset).getAmount(integer));
+            }
+        }
+        return Optional.empty();
+    }
+}
