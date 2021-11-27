93fbb3a1694d374d4e5bcd22d46ee9bc0eefc0f5
Matt <58913953+ms5984@users.noreply.github.com>
2021-11-27 01:02:43 -0500

    feat: add platform events; platform command impl
    * Events for Spigot and Bungee: AsyncTransactionEvent, BungeeTransactionEvent; very similar, but Bungee impl uses the built-in callback system to handle the final logging process; logging for Spigot relies on listener in the plugin impl
    * Create dynamic command bases: CustomCommand, BungeeCommand; move+copy prefixed-message util
    * Implement Bungee-side command: "genterprise", lists all Enterprise dependencies; TODO list registered ecos
    * Replace old command class in Spigot impl; TODO list registered ecos

--- Enterprise-Plugin/src/main/java/com/github/sanctum/economy/bungee/EnterpriseBungee.java
+++ Enterprise-Plugin/src/main/java/com/github/sanctum/economy/bungee/EnterpriseBungee.java
@@ -18,7 +18,97 @@
  */
 package com.github.sanctum.economy.bungee;
 
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * BungeeCord plugin implementation.
+ *
+ * @since 2.0.0
+ * @author ms5984
+ */
 public final class EnterpriseBungee extends Plugin {
+    static EnterpriseBungee instance;
+    String name;
+
+    @Override
+    public void onLoad() {
+        BungeeTransactionEvent.plugin = this;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        name = getDescription().getName();
+        getProxy().getPluginManager().registerCommand(this, new StaffCommand());
+    }
+
+    @Override
+    public void onDisable() {
+        getProxy().getPluginManager().unregisterCommands(this);
+    }
+
+    private class StaffCommand extends BungeeCommand {
+        StaffCommand() {
+            super("genterprise", "enterprise.staff", "proxyenterprise", "benterprise");
+        }
+
+        @Override
+        public void execute(CommandSender sender, String[] args) {
+            if (!hasPermission(sender)) {
+                return;
+            }
+            if (args.length == 0) {
+                // TODO: List registered ecos as well as any dependent plugins
+                getProxy().getScheduler().runAsync(EnterpriseBungee.this, new DependencyFinder(sender)::calculateAndSend);
+            }
+        }
+
+        @Override
+        public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+            return new ArrayList<>();
+        }
+
+        class DependencyFinder {
+            final CommandSender sender;
+            final Collection<Plugin> plugins;
+
+            DependencyFinder(CommandSender sender) {
+                this.sender = sender;
+                plugins = getProxy().getPluginManager().getPlugins();
+            }
+
+            void calculateAndSend() {
+                final List<String> collect = plugins.stream()
+                        .filter(this::depends)
+                        .map(this::simpleDescription)
+                        .collect(Collectors.toList());
+                if (collect.isEmpty()) {
+                    sender.sendMessage(TextComponent.fromLegacyText("&cNo detected Enterprise dependencies."));
+                    return;
+                }
+                final StringBuilder message = new StringBuilder("Detected plugins: ");
+                for (String desc : collect) {
+                    message.append(", ").append(desc);
+                }
+                sendMessage(sender, message.toString());
+            }
+
+            private boolean depends(Plugin plugin) {
+                return plugin.getDescription().getDepends().contains(name) || plugin.getDescription().getSoftDepends().contains(name);
+            }
+
+            private String simpleDescription(Plugin plugin) {
+                return plugin.getDescription().getName() + " "
+                        + plugin.getDescription().getVersion() + " by "
+                        + plugin.getDescription().getAuthor();
+            }
+        }
+    }
 }
---
+++ Enterprise-Spigot/src/main/java/com/github/sanctum/economy/spigot/AsyncTransactionEvent.java
@@ -0,0 +1,154 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.github.sanctum.economy.spigot;
+
+import com.github.sanctum.economy.transaction.*;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * A transaction event for Spigot plugins.
+ *
+ * @since 2.0.0
+ * @author ms5984
+ * @param <T> the transaction type
+ */
+public final class AsyncTransactionEvent<T extends MemoryTransaction> extends Event {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    static JavaPlugin plugin;
+    final T transactionInfo;
+    final AtomicBoolean logged = new AtomicBoolean(false);
+
+    /**
+     * Directly create a new transaction event.
+     * <p>
+     * Note: this constructor does not automatically call the event;
+     * if that is desired you must do so separately.
+     *
+     * @param transactionInfo a transaction
+     * @see #call(QueryTransaction)
+     * @see #call(ReceiveTransaction)
+     * @see #call(SetTransaction)
+     * @see #call(SourceTransaction)
+     * @see #call(TotalTransaction)
+     */
+    public AsyncTransactionEvent(@NotNull T transactionInfo) {
+        super(true);
+        this.transactionInfo = transactionInfo;
+    }
+
+    /**
+     * Get the transaction in this event.
+     *
+     * @return the transaction in this event
+     */
+    public T getTransactionInfo() {
+        return transactionInfo;
+    }
+
+    /**
+     * Whether this event should be logged to console.
+     *
+     * @return true if the event should be logged
+     */
+    public boolean isLogged() {
+        return logged.get();
+    }
+
+    /**
+     * Set whether this event should be logged to console.
+     *
+     * @param log whether to log
+     */
+    public void setLogged(boolean log) {
+        logged.set(log);
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction: " + transactionInfo.getDescription();
+    }
+
+    AsyncTransactionEvent<T> call() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> Bukkit.getPluginManager().callEvent(AsyncTransactionEvent.this));
+        return this;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    // for fevents contract
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    /**
+     * Create and call an event for a query operation.
+     *
+     * @param query a query transaction
+     * @return the scheduled-to-be-called event
+     */
+    public static AsyncTransactionEvent<QueryTransaction> call(@NotNull QueryTransaction query) {
+        return new AsyncTransactionEvent<>(query).call();
+    }
+
+    /**
+     * Create and call an event for a receive operation.
+     *
+     * @param give a give transaction
+     * @return the scheduled-to-be-called event
+     */
+    public static AsyncTransactionEvent<ReceiveTransaction> call(@NotNull ReceiveTransaction give) {
+        return new AsyncTransactionEvent<>(give).call();
+    }
+
+    /**
+     * Create and call an event for a set operation.
+     *
+     * @param set a set transaction
+     * @return the scheduled-to-be-called event
+     */
+    public static AsyncTransactionEvent<SetTransaction> call(@NotNull SetTransaction set) {
+        return new AsyncTransactionEvent<>(set).call();
+    }
+
+    /**
+     * Create and call an event for a source operation.
+     *
+     * @param take a take transaction
+     * @return the scheduled-to-be-called event
+     */
+    public static AsyncTransactionEvent<SourceTransaction> call(@NotNull SourceTransaction take) {
+        return new AsyncTransactionEvent<>(take).call();
+    }
+
+    /**
+     * Create and call an event for a total operation.
+     *
+     * @param total a total transaction
+     * @return the scheduled-to-be-called event
+     */
+    public static AsyncTransactionEvent<TotalTransaction> call(@NotNull TotalTransaction total) {
+        return new AsyncTransactionEvent<>(total).call();
+    }
+}
