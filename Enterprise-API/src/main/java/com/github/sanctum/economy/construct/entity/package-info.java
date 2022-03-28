8694bf678789774b0a663ccf102e309a82622c3c
Matt <58913953+ms5984@users.noreply.github.com>
2022-03-27 22:01:57 -0400

    feat(doc): packages
    * Draft package-info.java for assets, entity, system and impl

---
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/construct/entity/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * <h1>Entities</h1>
+ * Entities in Enterprise represent economy participants.
+ * <p>
+ * Traditional economy interfaces have used separate methods to expose
+ * the capabilities of each participant type--if they even have multiple
+ * participant types! This leads to duplication--duplicate methods for
+ * each type of participant and duplication of effort when wrappers are
+ * constructed to expose common functionalities.
+ * <p>
+ * <h3>Enterprise remedies this by fully separating participant identity
+ * and participant capabilities.</h3>
+ * This package focuses entirely on participant identity.
+ */
+package com.github.sanctum.economy.construct.entity;
---
+++ Enterprise-API/src/main/java/com/github/sanctum/economy/impl/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <h1>Implementations</h1>
+ * Provider implementations.
+ * <p>
+ * Developers must extend the classes found here to implement a provider.
+ * TODO how to get providers
+ *
+ * @since 2.0.0
+ */
+package com.github.sanctum.economy.impl;
