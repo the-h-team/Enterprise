ea268b987e98127cff0011f851d6707bf95443d3
Matt <58913953+ms5984@users.noreply.github.com>
2023-11-23 00:25:10 -0500

    feat: develop Transfer api
    * Transfer.Exception, Transfer.ExceptionalOutcome: enrich information provided by exceptional returns--enum provides a clean means to process+report on all exceptional result states under the same catch clause
    * Transfer#execute: support partial reversal using opportunistic cast in the case that the receiver fails to receive
    * Update Transfer.Reversible#execute impl; doc
    * TODO update Transfer.Reversible#reverse; make PendingResult calls chainable

--- api/src/main/java/io/github/sanctum/economy/construct/system/transactions/types/Transfer.java
+++ api/src/main/java/io/github/sanctum/economy/construct/system/transactions/types/Transfer.java
@@ -16,12 +16,16 @@
 package io.github.sanctum.economy.construct.system.transactions.types;
 
 import io.github.sanctum.economy.construct.assets.Amount;
+import io.github.sanctum.economy.construct.system.Resolvable;
 import io.github.sanctum.economy.construct.system.behaviors.Receiver;
 import io.github.sanctum.economy.construct.system.behaviors.Source;
+import io.github.sanctum.economy.construct.system.exceptions.ParticipantException;
 import io.github.sanctum.economy.construct.system.transactions.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
+
+import static io.github.sanctum.economy.construct.system.transactions.types.Transfer.ExceptionalOutcome.*;
 
 /**
  * Represents the transfer of a single asset between two participants.
@@ -55,6 +59,20 @@ public abstract class Transfer implements Transaction<Result.NotEmpty<Amount>, A
      */
     public abstract @NotNull Receiver getReceiver();
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method will first attempt to take the amount from the source
+     * and then attempt to give it to the receiver.
+     * <p>
+     * If either action fails, the transaction will fail.
+     *
+     * @return a pending result
+     * @implNote This implementation may make a best-effort attempt to reverse
+     * the {@code take} operation if the {@code give} operation fails, relying
+     * on the source's ability to be the target of a {@code give} operation.
+     * @see ExceptionalOutcome#GIVE_FAILED_TAKE_REVERSED
+     */
     @Override
     public @NotNull PendingResult<Result.NotEmpty<Amount>, Amount> execute() {
         final Source source = getSource();
@@ -64,9 +82,18 @@ public abstract class Transfer implements Transaction<Result.NotEmpty<Amount>, A
         sourcePendingResult.ifSuccessful(() -> {
             final PendingResult<? extends Result.NotEmpty<Receiver>, Receiver> receiverPendingResult = receiver.asyncGive(amount);
             receiverPendingResult.ifSuccessful(() -> cf.complete(Result.success(amount)));
-            receiverPendingResult.ifFailed(cf::completeExceptionally);
+            receiverPendingResult.ifFailed(giveError -> {
+                // attempt to refund source
+                if (source instanceof Receiver) {
+                    final PendingResult<? extends Result.NotEmpty<Receiver>, Receiver> sourceAsReceiver = ((Receiver) source).asyncGive(amount);
+                    sourceAsReceiver.ifSuccessful(() -> cf.completeExceptionally(new Exception(receiver, GIVE_FAILED_TAKE_REVERSED, giveError)));
+                    sourceAsReceiver.ifFailed(ignored -> cf.completeExceptionally(new Exception(receiver, GIVE_FAILED_TAKE_HOLDS, giveError)));
+                } else {
+                    cf.completeExceptionally(new Exception(receiver, GIVE_FAILED, giveError));
+                }
+            });
         });
-        sourcePendingResult.ifFailed(cf::completeExceptionally);
+        sourcePendingResult.ifFailed(e -> cf.completeExceptionally(new Exception(source, TAKE_FAILED, e)));
         return PendingResult.of(cf);
     }
 
@@ -89,19 +116,38 @@ public abstract class Transfer implements Transaction<Result.NotEmpty<Amount>, A
             super(amount);
         }
 
+        /**
+         * {@inheritDoc}
+         * <p>
+         * This method will first attempt to take the amount from the source
+         * and then attempt to give it to the receiver.
+         * <p>
+         * If either action fails, the transaction will fail.
+         *
+         * @return a pending result
+         * @implNote If the {@code take} operation succeeds but the {@code give}
+         * operation fails this method will make an attempt to reverse the
+         * {@code take} operation; in either case, the transaction will fail.
+         * @see ExceptionalOutcome#GIVE_FAILED_TAKE_REVERSED
+         * @see ExceptionalOutcome#GIVE_FAILED_TAKE_HOLDS
+         */
         @Override
         public @NotNull PendingResult<Result.NotEmpty<Amount>, Amount> execute() {
-            final Source originalSource = getOriginalSource();
-            final Receiver originalReceiver = getOriginalReceiver();
+            final Source source = getOriginalSource();
+            final Receiver receiver = getOriginalReceiver();
             final CompletableFuture<Result.NotEmpty<Amount>> cf = new CompletableFuture<>();
-            final PendingResult<? extends Result.NotEmpty<Source>, Source> sourcePendingResult = originalSource.asyncTake(getAmount());
+            final PendingResult<? extends Result.NotEmpty<Source>, Source> sourcePendingResult = source.asyncTake(getAmount());
             sourcePendingResult.ifSuccessful(() -> {
-                final PendingResult<? extends Result.NotEmpty<Receiver>, Receiver> receiverPendingResult = originalReceiver.asyncGive(getAmount());
+                final PendingResult<? extends Result.NotEmpty<Receiver>, Receiver> receiverPendingResult = receiver.asyncGive(getAmount());
                 receiverPendingResult.ifSuccessful(() -> cf.complete(Result.success(getAmount())));
-                receiverPendingResult.ifFailed(cf::completeExceptionally);
-                receiverPendingResult.ifFailed(r -> getOriginalSource().asyncGive(getAmount())); // best effort to refund the source
+                receiverPendingResult.ifFailed(giveError -> {
+                    // attempt to refund source
+                    final PendingResult<? extends Result.NotEmpty<Receiver>, Receiver> sourceAsReceiver = getOriginalSource().asyncGive(getAmount());
+                    sourceAsReceiver.ifSuccessful(() -> cf.completeExceptionally(new Exception(receiver, GIVE_FAILED_TAKE_REVERSED, giveError)));
+                    sourceAsReceiver.ifFailed(ignored -> cf.completeExceptionally(new Exception(receiver, GIVE_FAILED_TAKE_HOLDS, giveError)));
+                });
             });
-            sourcePendingResult.ifFailed(cf::completeExceptionally);
+            sourcePendingResult.ifFailed(e -> cf.completeExceptionally(new Exception(source, TAKE_FAILED, e)));
             return PendingResult.of(cf);
         }
 
@@ -157,6 +203,71 @@ public abstract class Transfer implements Transaction<Result.NotEmpty<Amount>, A
         @Deprecated
         public final @NotNull Receiver getReceiver() {
             return getOriginalReceiver();
+        }
+    }
+
+    /**
+     * The possible exceptional outcomes of a transfer.
+     * <p>
+     * Note that {@link #GIVE_FAILED_TAKE_REVERSED} is only possible
+     * if the transfer is reversible.
+     *
+     * @since 2.0.0
+     */
+    public enum ExceptionalOutcome {
+        TAKE_FAILED("not sent"),
+        GIVE_FAILED("sent but not received"),
+        GIVE_FAILED_TAKE_REVERSED("sent but not received; sender refunded"),
+        GIVE_FAILED_TAKE_HOLDS("sent but not received; sender not refunded"),
+        ;
+
+        private final String message;
+
+        ExceptionalOutcome(String message) {
+            this.message = message;
+        }
+    }
+
+    /**
+     * A system exception involving a transaction.
+     *
+     * @since 2.0.0
+     * @see ParticipantException
+     * @see Throwable#getCause()
+     */
+    public static final class Exception extends ParticipantException {
+        private static final long serialVersionUID = 5627984397988688466L;
+        private final ExceptionalOutcome outcome;
+
+        /**
+         * Constructs a new error with the given outcome and cause.
+         *
+         * @param participant the participant
+         * @param outcome an exceptional outcome
+         * @param cause the cause
+         */
+        private Exception(@NotNull Resolvable participant, ExceptionalOutcome outcome, Throwable cause) {
+            super(participant, "Transaction failed: " + outcome.message, cause);
+            this.outcome = outcome;
+        }
+
+        /**
+         * Gets the outcome.
+         * <p>
+         * This will reflect the state of the transaction at the time of the error.
+         * <p>
+         * As such, it will be any of the following:
+         * <ul>
+         *     <li>{@link ExceptionalOutcome#TAKE_FAILED}</li>
+         *     <li>{@link ExceptionalOutcome#GIVE_FAILED}</li>
+         *     <li>{@link ExceptionalOutcome#GIVE_FAILED_TAKE_REVERSED}</li>
+         *     <li>{@link ExceptionalOutcome#GIVE_FAILED_TAKE_HOLDS}</li>
+         * </ul>
+         *
+         * @return an outcome
+         */
+        public @NotNull Transfer.ExceptionalOutcome getOutcome() {
+            return outcome;
         }
     }
 }
