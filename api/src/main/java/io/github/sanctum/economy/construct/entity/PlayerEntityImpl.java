8cfeaa8213a8c6ee7479f82d64268925f91f51ef
Matt <58913953+ms5984@users.noreply.github.com>
2023-11-24 05:37:14 -0500

    fix: EnterpriseEntityImpl contract
    * all entities are based on EnterpriseEntityImpl--therefore, define and lock equals, hashCode behavior here (allowing subtypes to match as equal provided that namespace and identityKey match (we'll ignore toString)).
    * update PlayerEntityImpl (remove conflicting impl)

--- api/src/main/java/io/github/sanctum/economy/construct/entity/PlayerEntityImpl.java
+++ api/src/main/java/io/github/sanctum/economy/construct/entity/PlayerEntityImpl.java
@@ -45,22 +45,6 @@ class PlayerEntityImpl<T> extends EnterpriseEntityImpl implements PlayerEntity<T
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PlayerEntityImpl<?>)) return false;
-        if (!super.equals(o)) return false;
-        PlayerEntityImpl<?> that = (PlayerEntityImpl<?>) o;
-        return identifyingProperty.equals(that.identifyingProperty);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + identifyingProperty.hashCode();
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "PlayerEntity." + getClass().getSimpleName() + "{" +
                 "identityKey='" + identityKey + '\'' +
